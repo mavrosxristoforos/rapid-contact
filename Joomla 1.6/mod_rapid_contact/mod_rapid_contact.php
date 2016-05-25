427777eb1860f519e90797ae4eb4e2a48b42d714
mavrosxristoforos <mavrosxristoforos@34e49a50-83e2-49a1-8d36-ca667c9991bd>
2016-05-25 20:19:05 +0000

    Updated form, removed table, changed defaults, added captcha, added rpx ad

--- Joomla 1.6/mod_rapid_contact/mod_rapid_contact.php
+++ Joomla 1.6/mod_rapid_contact/mod_rapid_contact.php
@@ -3,14 +3,14 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 //Email Parameters
-$recipient = $params->get('email_recipient', '');
+$recipient = $params->get('email_recipient', 'email@email.com');
 $fromName = @$params->get('from_name', 'Rapid Contact');
 $fromEmail = @$params->get('from_email', 'rapid_contact@yoursite.com');
 
 // Text Parameters
-$myEmailLabel = $params->get('email_label', 'Email:');
-$mySubjectLabel = $params->get('subject_label', 'Subject:');
-$myMessageLabel = $params->get('message_label', 'Message:');
+$myEmailLabel = $params->get('email_label', 'email@site.com');
+$mySubjectLabel = $params->get('subject_label', 'Subject');
+$myMessageLabel = $params->get('message_label', 'Your Message');
 $buttonText = $params->get('button_text', 'Send Message');
 $pageText = $params->get('page_text', 'Thank you for your contact.');
 $errorText = $params->get('error_text', 'Your message could not be sent. Please try again.');
@@ -26,17 +26,10 @@ $emailWidth = $params->get('email_width', '15');
 $subjectWidth = $params->get('subject_width', '15');
 $messageWidth = $params->get('message_width', '13');
 $buttonWidth = $params->get('button_width', '100');
-$label_pos = $params->get('label_pos', '0');
-$addcss = $params->get('addcss', 'div.rapid_contact tr, div.rapid_contact td { border: none; padding: 3px; }');
-
-// URL Parameters
-$exact_url = $params->get('exact_url', true);
-$disable_https = $params->get('disable_https', true);
-$fixed_url = $params->get('fixed_url', true);
-$myFixedURL = $params->get('fixed_url_address', '');
+$label_pos = $params->get('label_pos', '2');
 
 // Anti-spam Parameters
-$enable_anti_spam = $params->get('enable_anti_spam', true);
+$enable_anti_spam = $params->get('enable_anti_spam', '1');
 $myAntiSpamQuestion = $params->get('anti_spam_q', 'How many eyes has a typical person?');
 $myAntiSpamAnswer = $params->get('anti_spam_a', '2');
 $anti_spam_position = $params->get('anti_spam_position', 0);
@@ -44,37 +37,20 @@ $anti_spam_position = $params->get('anti_spam_position', 0);
 // Module Class Suffix Parameter
 $mod_class_suffix = $params->get('moduleclass_sfx', '');
 
-
-if ($fixed_url) {
-  $url = $myFixedURL;
-}
-else {
-  if (!$exact_url) {
-    $url = JURI::current();
-  }
-  else {
-    if (!$disable_https) {
-      $url = (!empty($_SERVER['HTTPS'])) ? "https://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] : "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
-    }
-    else {
-      $url = "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
-    }
-  }
-}
-
-$url = htmlentities($url, ENT_COMPAT, "UTF-8");
+$url = $params->get('fixed_url', false) ? 'action="' . $params->get('fixed_url_address', '') . '"' : '';
 
 $myError = '';
 $CORRECT_ANTISPAM_ANSWER = '';
 $CORRECT_EMAIL = '';
 $CORRECT_SUBJECT = '';
 $CORRECT_MESSAGE = '';
+$email_class = '';
 
 if (isset($_POST["rp_email"])) {
   $CORRECT_SUBJECT = htmlentities($_POST["rp_subject"], ENT_COMPAT, "UTF-8");
   $CORRECT_MESSAGE = htmlentities($_POST["rp_message"], ENT_COMPAT, "UTF-8");
   // check anti-spam
-  if ($enable_anti_spam) {
+  if ($enable_anti_spam == '1') {
     if (strtolower($_POST["rp_anti_spam_answer"]) != strtolower($myAntiSpamAnswer)) {
       $myError = '<span style="color: ' . $error_text_color . ';">' . $wrongantispamanswer . '</span>';
     }
@@ -82,12 +58,23 @@ if (isset($_POST["rp_email"])) {
       $CORRECT_ANTISPAM_ANSWER = htmlentities($_POST["rp_anti_spam_answer"], ENT_COMPAT, "UTF-8");
     }
   }
+  else if ($enable_anti_spam == '2') {
+    // check captcha plugin.
+    JPluginHelper::importPlugin('captcha');
+    $d = JEventDispatcher::getInstance();
+    $res = $d->trigger('onCheckAnswer', 'not_used');
+    if( (!isset($res[0])) || (!$res[0]) ){
+      $myError = '<span style="color: ' . $error_text_color . ';">' . $wrongantispamanswer . '</span>';
+    }
+  }
   // check email
   if ($_POST["rp_email"] === "") {
     $myError = '<span style="color: ' . $error_text_color . ';">' . $noEmail . '</span>';
+    $email_class = ' has-error';
   }
   if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/", strtolower($_POST["rp_email"]))) {
     $myError = '<span style="color: ' . $error_text_color . ';">' . $invalidEmail . '</span>';
+    $email_class = ' has-error';
   }
   else {
     $CORRECT_EMAIL = htmlentities($_POST["rp_email"], ENT_COMPAT, "UTF-8");
@@ -126,48 +113,74 @@ if (isset($_POST["rp_email"])) {
 } // end if posted
 
 // check recipient
-if ($recipient === "") {
-  $myReplacement = '<span style="color: ' . $error_text_color . ';">No recipient specified</span>';
+if ($recipient === "email@email.com") {
+  $myReplacement = '<span style="color: ' . $error_text_color . ';">Your form recipient is email@email.com. Please change that in the Rapid Contact module options.</span>';
   print $myReplacement;
   return true;
 }
 
-print '<style type="text/css"><!--' . $addcss . '--></style>';
-print '<div class="rapid_contact ' . $mod_class_suffix . '"><form action="' . $url . '" method="post">' . "\n" .
+$document = JFactory::getDocument();
+$document->addStyleDeclaration('
+  .rapid_contact .form-control { max-width: 95%; }
+  .rapid_contact .g-recaptcha { margin-bottom: 10px; max-width: 95%; }
+');
+if ($params->get('addcss', '') != '') {
+  $document->addStyleDeclaration($params->get('addcss', ''));
+}
+
+$form_id = 'rp_'.uniqid();
+print '<div class="rapid_contact ' . $mod_class_suffix . '"><form '.$url.' id="'.$form_id.'" method="post">' . "\n" .
       '<div class="rapid_contact intro_text ' . $mod_class_suffix . '">'.$pre_text.'</div>' . "\n";
 
-if ($myError != '') {
-  print $myError;
-}
+if ($myError != '') { print $myError; }
 
-$separator = '</td><td>';
-$emptycell = '<td></td>';
-if ($label_pos == '1') {
-  $separator = '<br/>';
-  $emptycell = '';
-}
+print '<div class="rapid_contact_form" id="rapid_contact_form_'.$form_id.'">';
 
-print '<table>';
+$anti_spam_field = '';
+if ($enable_anti_spam == '2') {
+  $anti_spam_field = (JFactory::getConfig()->get('captcha') != '0') ? JCaptcha::getInstance(JFactory::getConfig()->get('captcha'))->display('rp_recaptcha', 'rp_recaptcha', 'g-recaptcha') : '';
+}
+else if ($enable_anti_spam == '1') {
+  // Label as Placeholder option is intentionally overlooked.
+  $anti_spam_field = '<label for="'.$form_id.'_as_answer">'.$myAntiSpamQuestion.'</label><input class="rapid_contact form-control inputbox ' . $mod_class_suffix . '" type="text" name="rp_anti_spam_answer" id="'.$form_id.'_as_answer" size="' . $emailWidth . '" value="'.$CORRECT_ANTISPAM_ANSWER.'"/>';
+}
 
 // print anti-spam
-if ($enable_anti_spam) {
-  if ($anti_spam_position == 0) {
-    print '<tr><td colspan="2">' . $myAntiSpamQuestion . '</td></tr><tr>'.$emptycell.'<td><input class="rapid_contact inputbox ' . $mod_class_suffix . '" type="text" name="rp_anti_spam_answer" size="' . $emailWidth . '" value="'.$CORRECT_ANTISPAM_ANSWER.'"/></td></tr>' . "\n";
-  }
+if ($anti_spam_position == 0) {
+  print '<div class="input-group">'.$anti_spam_field.'</div>';
 }
 // print email input
-print '<tr><td>' . $myEmailLabel . $separator . '<input class="rapid_contact inputbox ' . $mod_class_suffix . '" type="text" name="rp_email" size="' . $emailWidth . '" value="'.$CORRECT_EMAIL.'"/></td></tr>' . "\n";
+print '<div class="input-group">';
+$email_placeholder = ($label_pos == '2') ? ' placeholder="'.$myEmailLabel.'"' : '';
+if ($label_pos != '2') {
+  print '<label for="'.$form_id.'_email">'.$myEmailLabel.'</label>';
+}
+print '<input class="rapid_contact form-control inputbox ' . $email_class . $mod_class_suffix . '" type="email" name="rp_email" id="'.$form_id.'_email" size="'.$emailWidth.'" value="'.$CORRECT_EMAIL.'" '.$email_placeholder.'/>';
+print '</div>';
 // print subject input
-print '<tr><td>' . $mySubjectLabel . $separator . '<input class="rapid_contact inputbox ' . $mod_class_suffix . '" type="text" name="rp_subject" size="' . $subjectWidth . '" value="'.$CORRECT_SUBJECT.'"/></td></tr>' . "\n";
+print '<div class="input-group">';
+$subject_placeholder = ($label_pos == '2') ? ' placeholder="'.$mySubjectLabel.'"' : '';
+if ($label_pos != '2') {
+  print '<label for="'.$form_id.'_subject">'.$mySubjectLabel.'</label>';
+}
+print '<input class="rapid_contact form-control inputbox ' . $mod_class_suffix . '" type="text" name="rp_subject" id="'.$form_id.'_subject" size="'.$subjectWidth.'" value="'.$CORRECT_SUBJECT.'" '.$subject_placeholder.'/>';
+print '</div>';
 // print message input
-print '<tr><td valign="top">' . $myMessageLabel . $separator . '<textarea class="rapid_contact textarea ' . $mod_class_suffix . '" name="rp_message" cols="' . $messageWidth . '" rows="4">'.$CORRECT_MESSAGE.'</textarea></td></tr>' . "\n";
+print '<div class="input-group">';
+$message_placeholder = ($label_pos == '2') ? ' placeholder="'.$myMessageLabel.'"' : '';
+if ($label_pos != '2') {
+  print '<label for="'.$form_id.'_message">'.$myMessageLabel.'</label>';
+}
+print '<textarea class="rapid_contact form-control textarea ' . $mod_class_suffix . '" name="rp_message" id="'.$form_id.'_message" cols="' . $messageWidth . '" rows="4" '.$message_placeholder.'>'.$CORRECT_MESSAGE.'</textarea>';
+print '</div>';
 
 //print anti-spam
-if ($enable_anti_spam) {
-  if ($anti_spam_position == 1) {
-    print '<tr><td colspan="2">' . $myAntiSpamQuestion . '</td></tr><tr>'.$emptycell.'<td><input class="rapid_contact inputbox ' . $mod_class_suffix . '" type="text" name="rp_anti_spam_answer" size="' . $emailWidth . '" value="'.$CORRECT_ANTISPAM_ANSWER.'"/></td></tr>' . "\n";
-  }
+if ($anti_spam_position == 1) {
+  print '<div class="input-group">'.$anti_spam_field.'</div>';
 }
 // print button
-print '<tr><td colspan="2"><input class="rapid_contact button ' . $mod_class_suffix . '" type="submit" value="' . $buttonText . '" style="width: ' . $buttonWidth . '%"/></td></tr></table></form></div>' . "\n";
+print '<div class="input-group">';
+print '<input class="rapid_contact btn btn-primary button ' . $mod_class_suffix . '" type="submit" value="' . $buttonText . '"/>';
+print '</div>';
+print '</div></form></div>' . "\n";
 return true;
