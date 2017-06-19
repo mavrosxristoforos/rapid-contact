e18f6a27166925ab9cea9cac6acb37ba2ace1c8c
Christopher Mavros <mavrosxristoforos@gmail.com>
2017-06-19 09:48:01 +0000

    Updated PHP Info

--- mod_rapid_contact/mod_rapid_contact.php
+++ mod_rapid_contact/mod_rapid_contact.php
@@ -1,4 +1,14 @@
 <?php
+/*------------------------------------------------------------------------
+# mod_rapid_contact - Rapid Contact
+# ------------------------------------------------------------------------
+# author    Christopher Mavros - Mavrosxristoforos.com
+# copyright Copyright (C) 2008 Mavrosxristoforos.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://www.mavrosxristoforos.com
+# Technical Support:  Forum - http://www.mavrosxristoforos.com/support/forum
+-------------------------------------------------------------------------*/
+
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
